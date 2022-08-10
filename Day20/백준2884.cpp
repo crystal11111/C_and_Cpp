064c86b3bdb2a75c3492200fad78196dc3bbc4d7
Crystal Cho <78241019+crystal11111@users.noreply.github.com>
2022-08-10 18:54:56 +0900

    Update 백준2884.cpp

--- Day20/백준2884.cpp
+++ Day20/백준2884.cpp
@@ -1,25 +1,25 @@
-//#include <iostream>
-//using namespace std;
-//
-//int hour, minute;
-//
-//
-//int main() {
-//	cin >> hour >> minute;
-//	if (minute < 45) {
-//		minute = abs(minute + 60 - 45);
-//		hour = hour - 1;
-//			if (hour == -1) {
-//				hour = 23;
-//			}
-//			if (minute >= 45) {
-//				minute = minute - 45;
-//			}
-//
-//			cout << hour << minute;
-//
-//	}
-//
-//
-//	return 0;
-//}
+#include <iostream>
+using namespace std;
+
+int hour, minute;
+
+
+int main() {
+	cin >> hour >> minute;
+	if (minute < 45) {
+		minute = abs(minute + 60 - 45);
+		hour = hour - 1;
+			if (hour == -1) {
+				hour = 23;
+			}
+			if (minute >= 45) {
+				minute = minute - 45;
+			}
+
+			cout << hour << minute;
+
+	}
+
+
+	return 0;
+}
