b841e53bea67c97a03e07af0f80dd10f62f71033
Crystal Cho <78241019+crystal11111@users.noreply.github.com>
2022-08-10 18:54:31 +0900

    Update and rename Day20_EX2.c to Day20_EX3.c

--- Day20/Day20_EX3.c
+++ Day20/Day20_EX3.c
@@ -4,16 +4,14 @@ int main() {
 	char a[] = "apple";
 	char p[30] = "pine";
 
-	int size_a = sizeof(a);
-	printf("%d", size_a);
-
-	/*
+	int size_a = sizeof(a) - 1;
+	
 	for (int i = 0; i < 30; i++) {
 		
 	}
 
-	printf("%s", p); 
-	*/
+	printf("%s", p);	// pineapple github 체크
+
 
 	return 0;
 }
