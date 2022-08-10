e62e1bac1525a97514703949e7b0735a5a930d35
Crystal Cho <78241019+crystal11111@users.noreply.github.com>
2022-08-10 18:53:00 +0900

    Update Day20_EX1.cpp

--- Day20/Day20_EX1.cpp
+++ Day20/Day20_EX1.cpp
@@ -1,30 +1,30 @@
-//#include <iostream>
-//using namespace std;
-//
-//int Plus(int a, int b, int c) {
-//	return a + b + c;
-//}
-//
-//char Plus(int a, int b, int c) {		// ¿À¹ö·Îµù ¹İÈ¯ Å¸ÀÔ ±¸ºĞ X
-//	return a + b + c;
-//}
-///*
-//int Plus(int a, char b) {		// ÀÚ·áÇü
-//	return a + b;
-//}
-//
-//int Plus(char a, int b) {
-//	return a + b;
-//}
-//*/
-//
-//int main() {
-//	int result = Plus(1, 2, 3);
-//	printf("%d\n", result);
-//
-//	/*
-//	double result2 = Plus(3.0, 4.0);
-//	printf("%f\n", result2);
-//	*/
-//	return 0;
-//}
+#include <iostream>
+using namespace std;
+
+int Plus(int a, int b, int c) {
+	return a + b + c;
+}
+
+char Plus(int a, int b, int c) {		// ì˜¤ë²„ë¡œë”© ë°˜í™˜ íƒ€ì… êµ¬ë¶„ X
+	return a + b + c;
+}
+/*
+int Plus(int a, char b) {		// ìë£Œí˜•
+	return a + b;
+}
+
+int Plus(char a, int b) {
+	return a + b;
+}
+*/
+
+int main() {
+	int result = Plus(1, 2, 3);
+	printf("%d\n", result);
+
+	/*
+	double result2 = Plus(3.0, 4.0);
+	printf("%f\n", result2);
+	*/
+	return 0;
+}
